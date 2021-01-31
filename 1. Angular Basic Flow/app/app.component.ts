a0fae641c237be06aa177929ecec99796babedaa
Rohit Rao <rohit.rao.btech@gmail.com>
2021-01-31 15:55:04 +0530

    Pushing Basic flow Source Code - Initial Setup

---
+++ 1. Angular Basic Flow/app/app.component.ts
@@ -0,0 +1,47 @@
+/*
+  Component - It is a decorator. A decorator, is a function which annotates a set of functions,
+   Here we are using component decorator against typescript class, to tell Angular some specific metadata like template, 
+   selector for this class we need.
+*/
+import {Component} from '@angular/core' //#1 - 
+
+/*
+Now we can use a component decorator to implement metadata for the component we created,
+ It should be above your AppComponent class
+ @ - prefix for decorators
+*/
+@Component(                          //#3
+{
+  /*
+    selector creates a element in HTML.
+     app-root -> if you open index.html, you could see <app-root> , which is a base root component for each aangular app.
+  */
+  selector:'app-root', 
+
+  /*
+    Instead of template, we could use template URL which points to a HTML file
+    ticks - To have multiple line html block here
+  */
+  template:`
+    <div class="app">
+    {{title}}
+    </div>
+  
+  `,
+
+  //For your stylesheet
+  styleUrls:['app.component.scss']
+})
+
+/*
+Create a class with one property as Header
+*/
+export class AppComponent //#2
+{
+  header:string;
+  constructor()
+  {
+    this.header = 'Angular Basic Flow'
+  }
+
+}
---
+++ 1. Angular Basic Flow/app/app.module.ts
@@ -0,0 +1,64 @@
+/*
+  NgModule is a root module.
+*/
+
+import {NgModule} from '@angular/core'; //#6
+
+/*
+  Angular is splited in multiple modules,
+  So each piece of angular like forms, http lives inside different modules
+
+  As we are building browser app, we need to import BrowserModule from @angular/platform-browser
+
+  For browser modules, we also need to import commonModule from @angular/common
+*/
+import {BrowserModule} from '@angular/platform-browser'; //#10
+
+//Needed for browser module, gives us directive which will be used later in module
+import {CommonModule} from '@angular/common'; //#11 
+
+//Your custom component, imported AppComponent from app.component
+import {AppComponent} from './app.component'; //#5
+
+
+
+/*
+  NgModule is container that contains all of our components and depedencies such as services for http requests etc
+
+  Here we are telling NgModule to bootstrap our component
+*/
+@NgModule({ //#7
+
+//To register this AppComponent in module, We need to create decalration, its an Array
+declarations:[AppComponent] ,//#9
+
+/*
+  We need to import browser module and common module in NgModule decorator
+*/
+imports:[ BrowserModule, 
+          CommonModule], //#12
+
+//Starting point which instatntiate the angular app
+bootstrap:[AppComponent] //#8
+
+})
+
+
+/*
+It tells this AppModule is a NGModule - The decorator
+*/
+export class AppModule{} //#6
+
+
+/*
+#5  to #9 - If we see whole picture , We are importing AppComponent,
+            We are registering it and we are telling angular to bootstrap this component
+*/
+
+/*
+Fina;l - Angular will grab this NGModule, 
+find imports, 
+then boootstrap application based on appComponent,
+any further component lives inside this declaration section
+
+*/
